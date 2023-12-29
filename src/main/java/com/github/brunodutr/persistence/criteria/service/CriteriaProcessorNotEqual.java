056d0f3b68677b7761694f05d101c9579cc711a8
Bruno Dutra <brun_dutra@hotmail.com>
2023-12-29 11:06:42 -0300

    Update CriteriaProcessorNotEqual.java

--- src/main/java/com/github/brunodutr/persistence/criteria/service/CriteriaProcessorNotEqual.java
+++ src/main/java/com/github/brunodutr/persistence/criteria/service/CriteriaProcessorNotEqual.java
@@ -2,9 +2,9 @@ package com.github.brunodutr.persistence.criteria.service;
 
 import java.lang.reflect.Field;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 
 class CriteriaProcessorNotEqual implements ICriteriaProcessor {
 
