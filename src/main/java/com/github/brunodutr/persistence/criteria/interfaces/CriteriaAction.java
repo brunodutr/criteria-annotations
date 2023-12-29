76c5c623220b4105f9820131f5b76f0546dd9ccc
Bruno Dutra <brun_dutra@hotmail.com>
2023-12-29 11:10:08 -0300

    Update CriteriaAction.java

--- src/main/java/com/github/brunodutr/persistence/criteria/interfaces/CriteriaAction.java
+++ src/main/java/com/github/brunodutr/persistence/criteria/interfaces/CriteriaAction.java
@@ -2,7 +2,6 @@ package com.github.brunodutr.persistence.criteria.interfaces;
 import jakarta.persistence.criteria.Path;
 import jakarta.persistence.criteria.Predicate;
 
-
 @FunctionalInterface
 public interface CriteriaAction {
 
