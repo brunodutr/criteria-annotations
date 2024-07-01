31e25b07e302e3c63b217dab32d6ea3178e76aec
Bruno Dutra Franco <brun_dutra@hotmail.com>
2024-07-01 18:28:51 -0300

    fix(nested): criteria order by on nested fields

--- src/main/java/com/github/brunodutr/persistence/criteria/service/ICriteriaProcessor.java
+++ src/main/java/com/github/brunodutr/persistence/criteria/service/ICriteriaProcessor.java
@@ -19,17 +19,7 @@ interface ICriteriaProcessor {
 		Object value = CriteriaUtils.getValueWithReflection(object, field);
 
 		if (CriteriaUtils.hasValue(value)) {	
-			// case columnName contains a dot, it means that it is a nested column and we need to create a path to it (ex: user.name)
-			if (columnName.contains(".")) {
-				String[] columnNames = columnName.split("\\.");
-				Path<?> path = root.get(columnNames[0]);
-				for (int i = 1; i < columnNames.length; i++) {
-					path = path.get(columnNames[i]);
-				}
-				return criteriaAction.run(path, value);
-			}
-
-			Path<?> path = root.get(columnName);
+			Path<?> path = CriteriaUtils.getPath(root, columnName);
 			return criteriaAction.run(path, value);
 		}
 
